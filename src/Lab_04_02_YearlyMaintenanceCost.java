c158c9978cf239f696e390bdeb5c21e0367093aa
caleb <wambsgcc@mail.uc.edu>
2026-02-08 13:00:23 -0500

    Initial commit

---
+++ src/Lab_04_02_YearlyMaintenanceCost.java
@@ -0,0 +1,22 @@
+public class Lab_04_02_YearlyMaintenanceCost {
+    static void main(String[] args)
+
+    {
+        double spring = 108.85;
+        double summer = 217.27;
+        double fall = 86.72;
+        double winter = 230.51;
+        double yearlyMaint = spring + summer + fall + winter;
+
+
+        System.out.println("Your maintence was $" + spring + " in spring, $" + summer + " in summer, $" + fall + " in fall and $" + winter +" in winter. " );
+        System.out.println("Your total maintnence this year was $" + yearlyMaint + ".");
+
+
+
+
+    }
+
+
+
+}
